d6174ced71b739d137fbc82dfe181f12521380af
thi-rodrigues <thiagorodrigues1203@outlook.com>
2020-04-23 11:46:27 -0300

    Criado o componente HOME

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,7 +11,9 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatCardModule} from '@angular/material/card';
 import { MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
 
 
 @NgModule({
@@ -19,7 +21,8 @@ import { MatListModule} from '@angular/material/list';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +30,8 @@ import { MatListModule} from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
